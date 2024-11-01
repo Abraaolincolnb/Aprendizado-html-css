0c9ec721b1aaf67391fec6f56f3e5fac0c3db12e
Abraão Lincoln <abraao.bronzelli@gmail.com>
2024-11-01 00:19:54 -0300

    Exercicio 008
    Contem: desafio proposto, exemplos em js, e o exercicio feito na aula

---
+++ Javascript/exercicios/ex008/Exemplos.js
@@ -0,0 +1,14 @@
+/*var vel = 30
+console.log(`A velocidade do seu carro e ${vel}Km/Hr `)
+if (vel > 60){ // condição simples !!!
+    console.log("Voce esta muito rapido, TOMA LHE MULTA!")
+}
+console.log(`SE beber, não dirija`) 
+
+var pais = "Brasil"
+if(pais == "Brasil"){
+    console.log("Brasilandence")
+}
+else{
+    console.log("gringo!")
+}*/
