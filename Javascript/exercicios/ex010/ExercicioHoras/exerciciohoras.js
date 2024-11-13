756888aa91a13c14833762113c48ce756800f652
Abraão Lincoln <abraao.bronzelli@gmail.com>
2024-11-13 16:31:57 -0300

    decoraçã do menu
    e coclusão do exercicio 01

--- Javascript/exercicios/ex010/ExercicioHoras/exerciciohoras.js
+++ Javascript/exercicios/ex010/ExercicioHoras/exerciciohoras.js
@@ -1,20 +1,22 @@
 function carregar() {
 var msg = document.querySelector("div#msg")
-var img = document.getElementById('imagem')
+var img = document.querySelector("img#imagem")
 var data = new Date
 //var hora = data.getHours()
-var hora = 17
+var hora = 16
 var minuto = data.getMinutes()
 msg.innerHTML = `Agora são ${hora}:${minuto} Horas!`
-if (hora >= 0 && hora < 12){
+if (hora >= 6 && hora < 12){
     //bom dia
     img.src = "manha.png"
 } else if(hora >= 12 && hora < 18){
     //boa tarde
     img.src = "tarde.png"
+    document.body.style.background = "orange"
 }else{
     //boa noite 
     img.src = "noite.png"
+    document.body.style.background = "#2e2336"
 }
 
 
