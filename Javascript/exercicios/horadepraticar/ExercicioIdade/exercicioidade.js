69b9d53cb694d4534a501c3ef016e62b2da25df2
Abraão Lincoln <abraao.bronzelli@gmail.com>
2024-11-14 16:45:21 -0300

    produzindo o exercicio
    fazendo o js e o

---
+++ Javascript/exercicios/horadepraticar/ExercicioIdade/exercicioidade.js
@@ -0,0 +1,21 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.querySelector("input#txtano")
+    var res = document.querySelector("div#res")
+    if (fano.value.length == 0 || fano.value > ano){
+        alert("confere  dnv ai patrão ta tudo errado issae")
+    }else{
+        var fsex = document.getElementsByName("radsex")
+        var idade = ano - Number(fano.value)
+        var genero = ""
+        var img = document.createElement("img")
+        img.setAttribute('id', 'foto')
+        if(fsex[0].checked){
+            genero = "homem"
+        }else {
+            genero = "mulher"
+        }
+    }
+    res.innerHTML = `Detectamo ${genero} com ${idade} anos`
+}
