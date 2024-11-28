b853475465762e8a3ba06b00902ce2fc0fa41c92
Abraão Lincoln <abraao.bronzelli@gmail.com>
2024-11-28 16:01:17 -0300

    exercicio idades conclus'ao
    concloi

--- Javascript/exercicios/horadepraticar/ExercicioIdade/exercicioidade.js
+++ Javascript/exercicios/horadepraticar/ExercicioIdade/exercicioidade.js
@@ -13,10 +13,36 @@ function verificar() {
         img.setAttribute('id', 'foto')
         if(fsex[0].checked){
             genero = "homem"
+            if (idade >= 0 && idade < 10){
+                //crianca
+                img.setAttribute("src", "imagens/bebe-m.png")
+            }else if (idade >= 10 && idade < 21){
+                //jovem
+                img.setAttribute("src", "imagens/jovem-m.png")
+            } else if (idade >= 21 && idade < 50){
+                //adulto
+                img.setAttribute("src", "imagens/adulto-m.png")
+            } else {
+                img.setAttribute("src", "imagens/velho-m.png")
+            }
+                
         }else {
             genero = "mulher"
+            if (idade >= 0 && idade < 10){
+                //crinca
+                img.setAttribute("src", "imagens/bebe-f.png")
+            }else if (idade >= 10 && idade < 21){
+                //jovem
+                img.setAttribute("src", "imagens/jovem-f.png")
+            } else if (idade >= 21 && idade < 50){
+                //adulto
+                img.setAttribute("src", "imagens/adulta-f.png")
+            }else {
+                img.setAttribute("src", "imagens/velha-f.png")
+            }
         }
     }
     res.style.textAlign = "center"
     res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+    res.appendChild(img)
 }
